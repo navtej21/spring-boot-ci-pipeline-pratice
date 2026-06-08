6783334720e043bff7fd98af214e7042aa946ef5
nairnavtej <nairnavtej@gmail.com>
2026-06-08 17:02:14 +0530

    inital commit

---
+++ src/main/java/com/example/springbootpipeline/SpringbootpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpipelineApplication.class, args);
+	}
+
+}
