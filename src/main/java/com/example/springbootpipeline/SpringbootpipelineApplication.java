07b5b633134393972055b8bee7d3189498a90830
nairnavtej <nairnavtej@gmail.com>
2026-06-09 16:30:25 +0530

    added a change in the main class file

--- src/main/java/com/example/springbootpipeline/SpringbootpipelineApplication.java
+++ src/main/java/com/example/springbootpipeline/SpringbootpipelineApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootpipelineApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootpipelineApplication.class, args);
+		System.out.println("Hello World");
 	}
-
 }
+
